629aee52af544776d43deba2cf5065979dc3e333
Michael Griebling <sam@c-inspirations.com>
2023-03-20 16:46:48 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BigFloat
+
+A description of this package.
---
+++ Tests/BigFloatTests/BigFloatTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BigFloat
+
+final class BigFloatTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BigFloat().text, "Hello, World!")
+    }
+}
