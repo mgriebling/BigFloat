30b15cfecb8cddfa23a94500eabea1faae8d7efa
Michael Griebling <sam@c-inspirations.com>
2023-03-20 17:44:18 -0400

    Initial version of BigFloat Package.

--- Sources/BigFloat/BigFloat.swift
+++
@@ -1,6 +0,0 @@
-public struct BigFloat {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/BigFloatTests/BigFloatTests.swift
+++ Tests/BigFloatTests/BigFloatTests.swift
@@ -6,6 +6,11 @@ final class BigFloatTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BigFloat().text, "Hello, World!")
+        let pi = BigCFloat(piWithRadix: 10)!   //BigFloat(piWithRadix: 10)!
+        let two = BigCFloat(int: 2, radix: 10)!
+        let i = BigCFloat(real: BigFloat(), imaginary: BigFloat(int: 1, radix: 10))!
+        two.sqrt()
+        print(pi.toString!, two.toString!, i.toString!)
+        // XCTAssertEqual(BigFloat().text, "Hello, World!")
     }
 }
